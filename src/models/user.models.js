8d08c10eca2732f57809b2fd490827e17993a669
Mohammad Usman <uk351675@gmail.com>
2024-03-22 11:50:27 +0530

    user, video model complete,methods and hooks injected in model jwt,bcrypt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { jsonwebtoken as jwt } from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,7 +23,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password required!"],
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
     },
@@ -47,5 +49,39 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+///password ke regarding hooks hum istemal kar rahe hain jaise- password ko store karane se pehle use hash kar rahe he , aur password ka validation bhi
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+}); // this is a pre hook to hash the password from plain text
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}; // check password correctness
+
+// token generation
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      fullName: this.fullName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
 
 export const User = new mongoose.model("User", userSchema);
